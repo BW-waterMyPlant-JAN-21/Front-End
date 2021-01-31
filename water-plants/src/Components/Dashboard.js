697241523f9485e7c6e0a959bdc43c9e6762c64f
jayaramsivaramannair <jayaramsivaramannair@gmail.com>
2021-01-30 20:09:54 -0500

    Added the Functionality for deleting cards and added a counterdown timer left for hydrating plants

--- water-plants/src/Components/Dashboard.js
+++ water-plants/src/Components/Dashboard.js
@@ -1,52 +1,121 @@
-import React, {useState} from 'react';
-import axios from 'axios';
-import Plant from './Plant.js';
+import React, {useState} from "react";
+import axios from "axios";
+import Plant from "./Plant.js";
+import nature from "../Assets/nature.jpg";
+
 
 const Dashboard = (props) => {
-    const initialPlantDetails = {
-        nickname: " ",
-        species: " "
-    }
+  const initialPlantDetails = {
+    nickname: " ",
+    species: " ",
+    schedule: " ",
+    plantImage: nature,
+  };
 
-    const [plantDetails, setPlantDetails] = useState(initialPlantDetails);
-    const [userPlants, setUserPlants] = useState([]);
+  const [plantDetails, setPlantDetails] = useState(initialPlantDetails);
+  const [userPlants, setUserPlants] = useState([]);
 
-    const enterPlantDetails = (evt) => {
-        const{name, value} = evt.target;
-        console.log(name, value);
-        setPlantDetails({...plantDetails, [name]: value});
-    }
+  const enterPlantDetails = (evt) => {
+    const { name, value, type} = evt.target;
+    console.log(name, type, value);
+    const updatedValue = (type === 'file' ? evt.target.files[0] : value);
+    setPlantDetails({...plantDetails, [name]: updatedValue});
 
-    const createPlantCards = (evt) => {
-        evt.preventDefault();
+  };
 
-        axios.post("https://reqres.in/api/users", plantDetails)
-        .then((res) => setUserPlants([...userPlants, plantDetails]))
-        .catch((err) => console.log(err))
+  const createPlantCards = (evt) => {
+    evt.preventDefault();
 
-        setPlantDetails(initialPlantDetails);
-    }
+    axios
+      .post("/users", plantDetails)
+      .then((res) => {
+        console.log(res.data)
+        setUserPlants([...userPlants, res.data])
+      })
+      .catch((err) => console.log(err));
 
-    return (
+    setPlantDetails(initialPlantDetails);
+  };
+
+  const triggerDelete = ((index) => {
+      let copyUserPlants = [...userPlants];
+      copyUserPlants.splice(index, 1);
+      setUserPlants(copyUserPlants)
+  })
+
+  return (
+    <div>
+      <h2>Add Your Plants!</h2>
+      <form onSubmit={createPlantCards} className='plantForm'>
+        <label>
+          nickname:
+          <input
+            onChange={enterPlantDetails}
+            type='text'
+            value={plantDetails.nickname}
+            name='nickname'
+            placeholder='Enter nickname'
+          />
+        </label>
+        <label>
+          species:
+          <input
+            onChange={enterPlantDetails}
+            type='text'
+            value={plantDetails.species}
+            name='species'
+            placeholder='Enter species'
+          />
+        </label>
         <div>
-            <h2>Add Your Plants!</h2>
-            <form onSubmit = {createPlantCards} className="plantForm">
-                <label>nickname:
-                    <input onChange = {enterPlantDetails} type="text" value={plantDetails.nickname} name="nickname" placeholder="Enter nickname"/>
-                </label>
-                <label>species:
-                    <input onChange = {enterPlantDetails} type="text" value= {plantDetails.species} name="species" placeholder="Enter species"/>
-                </label>
-                <button>Add The Plant</button>
-            </form>
-            <div className="plantContainer">
-                {userPlants.map((plant) => {
-                    return <Plant nickname={plant.nickname} species={plant.species}/>
-                    })
-                }
-            </div>
+            <p>H2O Frequency Timer:</p>
+          <label>
+            Set the Schedule
+            <select onChange = {enterPlantDetails} value = {plantDetails.schedule} name="schedule">
+              <option value="Every Minute">Every Minute</option>
+              <option value="Hourly">Hourly</option>
+              <option value="Daily">Daily</option>
+              <option value="Weekly">Weekly</option>
+              <option value="Bi Weekly">Bi-Weekly</option>
+              <option value="Every 10 Days">Every 10 Days</option>
+            </select>
+          </label>
         </div>
-    )
-}
+        <label>Select an Image
+          <div>
+            <input type="file" onChange ={enterPlantDetails} name="plantImage"/>
+          </div>
+        </label>
+        <button>Add The Plant</button>
+      </form>
+      <div className='plantContainer'>
+        {userPlants.map((plant,index) => {
+          return (
+            <Plant
+              key = {index}
+              nickname={plant.nickname}
+              species={plant.species}
+              data={userPlants}
+              countDownDate = {
+                (() => {
+                  switch(plant.schedule) {
+                    case 'Daily': return new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate() + 1).getTime() ;
+                    case 'Weekly': return new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate() + 7).getTime();
+                    case 'Bi Weekly': return new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate() + 14).getTime();
+                    case 'Every 10 Days': return new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate() + 10).getTime();
+                    case 'Hourly': return new Date().getTime() + (60 * 60 * 1000);
+                    default: return new Date().getTime() + (60 * 1000);
+                  }
+                })()
+              }
+              image = {plant.plantImage}
+              triggerDelete = {triggerDelete}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
 export default Dashboard;
--- water-plants/src/Components/Plant.js
+++ water-plants/src/Components/Plant.js
@@ -1,58 +1,84 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import nature from '../Assets/nature.jpg';
-
-const useStyles = makeStyles({
-    root: {
-      maxWidth: 150,
-    },
-    media: {
-      height: 100,
-      width: '150%',
-    },
-  });
-
-const clickFunction = (evt) => {
-    console.log(`you clicked ${evt.target}`)
-}
+import React, {useState, useEffect, useRef} from 'react';
+import {Card, CardBody, CardLink, CardTitle, CardSubtitle} from 'reactstrap';
 
 const Plant = (props) => {
-    const {nickname, species} = props;
-    const classes = useStyles();
+    const {nickname, species, data, triggerDelete, image, countDownDate} = props;
+
+    const [timer, setTimer] = useState({
+        daysLeft: "00",
+        hoursLeft: "00", 
+        minutesLeft: "00", 
+        secondsLeft: "00",
+    })
+
+    let interval = useRef();
+
+    const startTimer = () => {
+        const now = new Date().getTime()
+
+        interval = setInterval(() => {
+            const distance = countDownDate - now;
+
+            const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+            const hours = Math.floor((distance % (1000 * 60 * 60 * 24) / (1000 * 60 * 60)));
+            const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+            const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+            if(distance < 0) {
+                clearInterval(interval);
+            } else {
+                setTimer({...timer, daysLeft: days, hoursLeft: hours, minutesLeft: minutes, secondsLeft: seconds});
+            }
+
+        }, 1000);
+    };
+
+    useEffect(() => {
+        startTimer();
+        return () => {
+            clearInterval(interval);
+        };
+    });
+
+    const deleteFunction = (evt) => {
+    const nickNameText = evt.target.parentNode.parentNode.querySelector('h6').textContent;
+        
+        let datumIndex = 0;
+        data.filter((datum) => {
+            if(nickNameText.includes(datum.nickname)) {
+                datumIndex = data.indexOf(datum);
+                triggerDelete(datumIndex, 1);
+            }
+            return data;
+        })
+    }
+
     return (
         <div className="plantContainer">
-            <Card className={classes.root}>
-                <CardActionArea>
-                    <CardMedia
-                    className={classes.media}
-                    image={nature}
-                    title="plants"
-                    />
-                    <CardContent>
-                    <Typography gutterBottom variant="h6" component="h2">
-                        Species: {species}
-                    </Typography>
-                    <Typography variant="body2" color="textSecondary" component="p">
-                        Nickname: {nickname}
-                    </Typography>
-                    </CardContent>
-                </CardActionArea>
-                <CardActions>
-                    <Button onClick={clickFunction}size="small" color="primary">
-                        Update
-                    </Button>
-                    <Button size="small" color="primary">
+            <Card className="card">
+                <CardBody>
+                    <CardTitle tag="h5">{`Species: ${species}`}</CardTitle>
+                    <CardSubtitle tag="h6" className="mb-2 text-muted">{`Nickname: ${nickname}`}</CardSubtitle>
+                    </CardBody>
+                        <img width="100%" src={image} alt="nature" />
+                    <CardBody>
+                    <h3>Time Left to Water: </h3>
+                    <div className = 'timer'>
+                    <p>{timer.daysLeft}</p>
+                    <p>{` : `}</p>
+                    <p>{timer.hoursLeft}</p>
+                    <p>{` : `}</p>
+                    <p>{timer.minutesLeft}</p>
+                    <p>{` : `}</p>
+                    <p>{timer.secondsLeft}</p>
+                    </div>
+                    <button>Reset Timer</button>
+                    <button onClick={deleteFunction}size="small" color="primary">
                         Delete
-                    </Button>
-                </CardActions>
-            </Card>
+                    </button>
+                    <CardLink href="#">More Info</CardLink>
+                </CardBody>
+            </Card>      
         </div>
     )
 }
